68fc797b64613af5019092ac3a7f8e008d354a49
Cossetteg <10838257@uvu.edu>
2024-09-12 19:48:38 -0600

    Phase D
    momentum

---
+++ physics.py
@@ -0,0 +1,16 @@
+#Ok physics time
+'''
+Newtons laws:
+1- An object in motion will stay in motion
+2- Force = mass times acceleration
+3- Every action has an equal and opposite reaction
+'''
+
+def momentum(mass, speed):
+    return mass * speed
+
+def delta_momentum(mass, velocityf, velocityi):
+    return (mass * velocityf) - (mass * velocityi)
+
+def impulse():
+    delta_momentum()
