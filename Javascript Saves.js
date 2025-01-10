8cae56071d58d94259b84c7b44e08340192143fa
Cossette Gomez <10838257@my.uvu.edu>
2025-01-09 22:53:16 -0700

    environment setup

---
+++ Javascript Saves.js
@@ -0,0 +1,5 @@
+//Spring Semester 2025
+//Jan 9
+const words = ['spray', 'limit', 'elite', 'exuberant', 'malady', 'destruction', 'perfection', 'present']
+const result = words.filter(word => word.length > 6)
+console.log(result);
